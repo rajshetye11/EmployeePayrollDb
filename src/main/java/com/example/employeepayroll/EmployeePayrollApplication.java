2071eb0b87395962b6f37b14ee8a5f35250dc2bf
rajshetye11 <rajshetye11@gmail.com>
2021-10-02 00:13:48 +0530

    [raj] : added ability to use Lombok library for logging

--- src/main/java/com/example/employeepayroll/EmployeePayrollApplication.java
+++ src/main/java/com/example/employeepayroll/EmployeePayrollApplication.java
@@ -3,11 +3,15 @@ package com.example.employeepayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.error("Error generated ");
 	}
 
 }
