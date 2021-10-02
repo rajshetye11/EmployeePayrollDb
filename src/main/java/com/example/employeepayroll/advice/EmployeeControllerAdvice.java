6062f58dd06c00bc2407dd575ad70a31ee64672a
rajshetye11 <rajshetye11@gmail.com>
2021-10-03 00:12:51 +0530

    [raj] : added user friendly error response in case validation fails

---
+++ src/main/java/com/example/employeepayroll/advice/EmployeeControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.example.employeepayroll.advice;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class EmployeeControllerAdvice {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> notAllowed(MethodArgumentNotValidException ex) {
+		return new ResponseEntity<String>("Please enter valid Name", HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<String> notEmpty(HttpMessageNotReadableException exception) {
+		return new ResponseEntity<String>("Message cant be Empty", HttpStatus.NOT_FOUND);
+
+	}
+	
+	
+
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> noIdPresent(NoSuchElementException exception) {
+		return new ResponseEntity<String>("No id present", HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<String> badReq(HttpRequestMethodNotSupportedException exception) {
+		return new ResponseEntity<String>("Change HttpRequest", HttpStatus.BAD_REQUEST);
+	}
+}
